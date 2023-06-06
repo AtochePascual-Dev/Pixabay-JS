3018212f33ebe03a60929a2b1c9735e85ca8e779
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-05 18:04:44 -1000

    [ADD] mostrarAlerta

--- js/app.js
+++ js/app.js
@@ -19,6 +19,27 @@ const validarFormulario = (event) => {
   const terminoBusqueda = document.querySelector('#termino').value;
 
   if (terminoBusqueda === '') {
+    mostrarAlerta('Error Agregue un termino de búsqueda');
     return;
+  }
+};
+
+
+
+// * Muestra una alerta en pantalla 
+const mostrarAlerta = (mensaje) => {
+  const existeAlerta = document.querySelector('.alerta');
+
+  if (!existeAlerta) {
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+    alerta.classList.add('alerta', 'bg-red-100', 'border-red-400', 'font-bold', 'text-red-700', 'px-4', 'py-3', 'rounded', 'mt-6', 'mx-auto', 'max-w-lg', 'text-center');
+
+    formulario.appendChild(alerta);
+
+    // Eliminamos la alerta
+    setTimeout(() => {
+      alerta.remove();
+    }, 2000);
   }
 };

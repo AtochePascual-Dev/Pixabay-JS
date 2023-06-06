107957154df69a6aaad02d50c099ca2b37a62432
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-06 08:20:13 -1000

    [ADD] mostrarImagenes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // * Variables
 const formulario = document.querySelector('#formulario');
 const resultados = document.querySelector('#resultado');
-
+const registroPorPagina = 40;
 
 // * Eventos
 // * Caundo el documento esta listo
@@ -51,9 +51,40 @@ const mostrarAlerta = (mensaje) => {
 // * Busca las imagenes en la API
 const buscarImagenes = (terminoBusqueda) => {
   const key = '37058740-fc59b10279e7ee3461bcbab29';
-  const URL = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}`;
+  const URL = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}&per_page=100`;
 
   fetch(URL)
     .then(respueta => respueta.json())
-    .then(resultado => console.log(resultado))
+    .then(resultado => mostrarImagenes(resultado.hits))
+};
+
+
+
+// * Muestra las imagenes en pantalla
+const mostrarImagenes = (imagenes) => {
+
+  // Limpiamos los resultados previos
+  while (resultados.firstChild) {
+    resultados.firstChild.remove();
+  }
+
+  // Iteramos sobre las imagenes
+  imagenes.forEach(imagen => {
+    const { previewURL, likes, views, largeImageURL } = imagen;
+
+    resultados.innerHTML += `
+    <div class="w-1/2 md:w-1/3 lg:w-1/4">
+      <div class="bg-white m-2">
+        <img class="w-full h-56 object-cover" src="${previewURL}">
+        <div class="p-2">
+          <p class="font-bold">${likes} <span class="font-light">Me Gusta</span></p>
+          <p class="font-bold">${views} <span class="font-light">Vistas</span></p>
+          <a href="${largeImageURL}" class="block w-full bg-blue-800 hover:bg-blue-500 text-white text-center uppercase font-bold rounded p-1 mt-2" target="_blank" rel="noopener noreferrer">
+          Ver Imagen HD
+          </a>
+        </div>
+      </div>
+    </div>
+    `;
+  });
 };
